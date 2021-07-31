dd89996968235b738760703676dc2dd08dbadc5d
KID <50666207+KID421@users.noreply.github.com>
2021-07-31 19:32:27 +0800

    角色移動

--- 2DGame/Assets/Scripts/Player.cs
+++ 2DGame/Assets/Scripts/Player.cs
@@ -12,22 +12,64 @@ public class Player : MonoBehaviour
     [Header("是否在地板上"), Tooltip("用來儲存角色是否在地板上的資訊，在地板上 true、不在地板上 false")]
     public bool isGround;
 
+    // 私人欄位不顯示
+    // 開啟屬性面板除錯模式 Debug 可以看到私人欄位
     private AudioSource aud;
     private Rigidbody2D rig;
     private Animator ani;
+    /// <summary>
+    /// 玩家水平輸入值
+    /// </summary>
+    private float hValue;
     #endregion
 
+
     #region 事件
+    private void Start()
+    {
+        // GetComponent<類型>() 泛型方法，可以指定任何類型
+        // 作用：取得此物件的 2D 剛體元件
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // 一秒約執行 60 次
+    private void Update()
+    {
+        GetPlayerInputHorizontal();
+    }
+
+    // 固定更新事件
+    // 一秒固定執行 50 次，官方建議有使用到物理 API 要在此事件內執行
+    private void FixedUpdate()
+    {
+        Move(hValue);
+    }
     #endregion
 
     #region 方法
+    /// <summary>
+    /// 取得玩家輸入水平軸向值：A、D、左、右
+    /// </summary>
+    private void GetPlayerInputHorizontal()
+    {
+        // 水平值 = 輸入.取得軸向(軸向名稱)
+        // 作用：取得玩家按下水平按鍵的值，按右為 1，按左為 -1，沒按為 0
+        hValue = Input.GetAxis("Horizontal");
+    }
+
     /// <summary>
     /// 移動
     /// </summary>
     /// <param name="horizontal">左右數值</param>
     private void Move(float horizontal)
     {
-
+        // 區域變數：在方法內的欄位，有區域性，僅限於此方法內存取
+        // 簡寫：transform 此物件的 Transform 變形元件
+        // posMove = 角色當前座標 + 玩家輸入的水平值
+        // Time.fixedDeltaTime 指 1/50 秒
+        Vector2 posMove = transform.position + new Vector3(horizontal, 0, 0) * speed * Time.fixedDeltaTime;
+        // 剛體.移動座標(要前往的座標)
+        rig.MovePosition(posMove);
     }
 
     /// <summary>
